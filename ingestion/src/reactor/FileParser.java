033640acafe298df7f2ba995ed1b444021a90eff
Jorge Luis Lazo <jorge.lazo@alumni.ubc.ca>
2016-05-25 12:16:10 -0700

    added indexer for pathwayRun using univocity and guava

--- ingestion/src/reactor/FileParser.java
+++ ingestion/src/reactor/FileParser.java
@@ -2,11 +2,14 @@ package reactor;
 
 import com.univocity.parsers.tsv.TsvParser;
 import com.univocity.parsers.tsv.TsvParserSettings;
+import org.javatuples.Pair;
+import org.javatuples.Quartet;
+import org.javatuples.Sextet;
+
 
 import java.io.File;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -15,6 +18,19 @@ import java.util.List;
  * Created by jorgeluis on 20/05/16.
  */
 class FileParser {
+//    private TsvParserSettings settings;
+    private TsvParser parser;
+
+    FileParser(){
+        TsvParserSettings settings = new TsvParserSettings();
+        //the file used here uses '\n' as the line separator sequence.
+        //the line separator sequence is defined here to ensure systems such as MacOS and Windows
+        //are able to process this file correctly (MacOS uses '\r'; and Windows uses '\r\n').
+        settings.getFormat().setLineSeparator("\n");
+
+        // creates a TSV parser
+        parser = new TsvParser(settings);
+    }
 
     public void parse(){
         TsvParserSettings settings = new TsvParserSettings();
@@ -24,7 +40,7 @@ class FileParser {
         settings.getFormat().setLineSeparator("\n");
 
         // creates a TSV parser
-        TsvParser parser = new TsvParser(settings);
+        parser = new TsvParser(settings);
 
         // parses all rows in one go.
         List<String[]> allRows = parser.parseAll(new File("/home/jorgeluis/Documents/Hallam/AlyseData/SI4096441_results/annotation_table/COG_stats_1.txt"));
@@ -38,9 +54,78 @@ class FileParser {
     private void prettyPrint(String[] row){
         StringBuilder output = new StringBuilder();
         for (int i = 0; i <row.length ; i++){
-            output.append(row[i] + ',');
+            output.append(row[i]);
         }
         System.out.println(output);
     }
+
+     Quartet getCogStats1(File file){
+        List<String[]> allRows = parser.parseAll(file);
+        return new Quartet<>(allRows.get(1)[1],allRows.get(2)[1], allRows.get(3)[1], allRows.get(4)[1]);
+    }
+
+    List<Pair> getCogStats2(File file){
+        List<Pair> result = new ArrayList<>();
+        List<String[]> allRows = parser.parseAll(file);
+
+        for (int i=1; i<= 25; i++){
+            String name = allRows.get(i)[0] + "_cog2";
+            Pair<String, String> tuple = Pair.with(name , allRows.get(i)[2]);
+            result.add(tuple);
+        }
+        return result;
+    }
+
+    Sextet getKeggStats1(File file){
+        List<String[]> allRows = parser.parseAll(file);
+        return new Sextet<>(
+                allRows.get(1)[1],
+                allRows.get(2)[1],
+                allRows.get(3)[1],
+                allRows.get(4)[1],
+                allRows.get(5)[1],
+                allRows.get(6)[1]
+        );
+    }
+
+    List<Pair> getKeggStats2(File file){
+        List<Pair> result = new ArrayList<>();
+        List<String[]> allRows = parser.parseAll(file);
+
+        for (int i=1; i<= 37; i++){
+            String name = modifyName(allRows.get(i)[0]);
+            Pair<String, String> tuple = Pair.with(name , allRows.get(i)[1]);
+            result.add(tuple);
+        }
+
+        return result;
+    }
+
+    private static String modifyName(String title){
+        StringBuilder result = new StringBuilder();
+
+        int len = title.length();
+        if(len < 25){ //parse the smaller titles first
+            result = result.append(title.trim().replaceAll(" ", "_").toLowerCase()).append("_kegg2");
+            return result.toString();
+        }
+        //specific cases
+        if(len == 40){
+            return "metabolism_terpenoids_kegg2";
+        }
+        if(len == 31){
+            return "metabolism_other_kegg2";
+        }
+        if(len == 36){
+            return "metabolism_cofactors_kegg2";
+        }
+        //others
+        else{
+            String[] words = title.split(" ");
+            result = result.append(words[0].replaceAll(",","").toLowerCase()).append("_kegg2");
+            return result.toString();
+        }
+    }
+
 
 }
--- ingestion/src/reactor/Main.java
+++ ingestion/src/reactor/Main.java
@@ -3,9 +3,16 @@ package reactor;
 
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.common.SolrInputDocument;
+import com.google.common.io.Files;
+import org.javatuples.Pair;
+import org.javatuples.Quartet;
+import org.javatuples.Sextet;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by jorgeluis on 20/05/16.
@@ -17,11 +24,77 @@ public class Main {
     public static Collection<SolrInputDocument> collection;
 
     public static void main(String[] args) throws IOException, SolrServerException {
+        boolean isDirectory = (new File(args[0]).isDirectory());
+        if (!isDirectory){
+            System.out.println("\n\nERROR: Invalid arguments passed.");
+            System.out.println("\n\nUSAGE: \n\targ[0]: Absolute path to folder containing a metaPathwayRun.");
+            throw new NoSuchFileException(printArgs(args));
+        }
+
+        //initializations
+        File baseDir = new File(args[0]);
         client = new SolrLink("pathwayRuns");
         parser = new FileParser();
+        //for each pathway run
 
-        System.out.println("Num records in Core: " + client.getCount());
+        for( File pathwayRun : Files.fileTreeTraverser().children(baseDir)){
 
-        parser.parse();
+            SolrInputDocument document = new SolrInputDocument();
+            for (File f : Files.fileTreeTraverser().preOrderTraversal(pathwayRun)){
+                importFile(f, document);
+            }
+        }
+        //parser.parse();
+    }
+
+    static private void importFile(File file, SolrInputDocument document){
+        String name = file.getName();
+//        System.out.println(file.getName());
+
+        switch (name){
+            case "COG_stats_1.txt":
+                Quartet cogStats1 = parser.getCogStats1(file);
+                document.addField("poorly_char_cog1", cogStats1.getValue0());
+                document.addField("info_process_cog1", cogStats1.getValue1());
+                document.addField("metabolism_cog1", cogStats1.getValue2());
+                document.addField("cell_signal_cog1", cogStats1.getValue3());
+                break;
+            case "COG_stats_2.txt":
+                List<Pair> cogStats2 = parser.getCogStats2(file);
+                for (Pair tuple : cogStats2){
+                    document.addField((String) tuple.getValue0(), tuple.getValue1());
+                }
+                break;
+            case "KEGG_stats_1.txt":
+                Sextet keggStats1 = parser.getKeggStats1(file);
+                document.addField("cell_process_kegg1", keggStats1.getValue0());
+                document.addField("human_disease_kegg1", keggStats1.getValue1());
+                document.addField("gene_info_kegg1", keggStats1.getValue2());
+                document.addField("environmental_info_kegg1", keggStats1.getValue3());
+                document.addField("organism_sys_kegg1", keggStats1.getValue4());
+                document.addField("metabolism_kegg1", keggStats1.getValue5());
+                break;
+            case "KEGG_stats_2.txt":
+                List<Pair> keggStats2 = parser.getKeggStats2(file);
+                for (Pair tuple : keggStats2){
+                    document.addField((String) tuple.getValue0(), tuple.getValue1());
+                }
+                break;
+        }
+
+    }
+
+    /**
+     * Method to pretty print arguments for debugging.
+     */
+    private static String printArgs(String[] arguments){
+        String result = "";
+        for (String arg: arguments){
+            result += arg + "\n";
+        }
+        if (result.equals("")){
+            return "NO ARGUMENTS SUPPLIED";
+        }
+        return result;
     }
 }
